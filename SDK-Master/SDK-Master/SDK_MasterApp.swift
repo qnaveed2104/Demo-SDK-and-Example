24f305049f9b11ed780bf18f87b52a06e6859afd
qazi <qazi.naveed@nextgeni.com>
2025-01-01 10:06:18 +0100

    setting up project

---
+++ SDK-Master/SDK-Master/SDK_MasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDK_MasterApp.swift
+//  SDK-Master
+//
+//  Created by Qazi on 01/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct SDK_MasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SDK-Master/SDK-MasterTests/SDK_MasterTests.swift
@@ -0,0 +1,17 @@
+//
+//  SDK_MasterTests.swift
+//  SDK-MasterTests
+//
+//  Created by Qazi on 01/01/2025.
+//
+
+import Testing
+@testable import SDK_Master
+
+struct SDK_MasterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
