499117ce39568376829bbdd53e9a08c80a388a74
qazi <qazi.naveed@nextgeni.com>
2025-01-01 12:45:18 +0100

    add example app

---
+++ ExampleApp/ExampleApp/ExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleAppApp.swift
+//  ExampleApp
+//
+//  Created by Qazi on 01/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
